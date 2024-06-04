c72b02f821d966180bc203e1816ab915e411a394
kimjeong_1 <59051200+kimjeong1@users.noreply.github.com>
2024-06-04 22:24:57 +0900

    Update GraphicsDrawLineEx.java

--- chapter12/GraphicsDrawLineEx.java
+++ chapter12/GraphicsDrawLineEx.java
@@ -16,6 +16,54 @@ public class GraphicsDrawLineEx extends JFrame {
             g.drawLine(20, 20, 100, 100); // 선그리기
         }
     }
+    // 다른 도형 예제
+    /*
+    class MyPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.drawOval(20,20,80,80);
+        }
+    }
+     */
+    /*
+    class MyPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.drawRect(20,20,80,80);
+        }
+    }
+     */
+    /*
+    class MyPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.drawRoundRect(20,20,120,80,40,60);
+        }
+    }
+     */
+    /*
+    class MyPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.drawArc(20,100,80,80,90,270);
+        }
+    }
+     */
+    /*
+    class MyPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        int[] x = {80,40,80,120};
+        int[] y = {40,120,200,120};
+        g.drawPolygon(x, y, 4);
+        }
+    }
+     */
     public static void main(String [] args) {
         new GraphicsDrawLineEx();
     }
